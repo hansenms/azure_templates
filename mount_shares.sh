b401acddf5e33c7658c9a173cbcdbf186800f3ef
Michael S. Hansen <michael.hansen@nih.gov>
2016-03-04 10:26:55 -0500

    Updated mount script

--- mount_shares.sh
+++ mount_shares.sh
@@ -9,6 +9,6 @@ apt-get -qq install cifs-utils -y
 mkdir -p /mnt/gtlog
 mkdir -p /mnt/gtdependencies 
 echo "${logshare} /mnt/gtlog cifs vers=3.0,username=${shareaccount},password=${sharekey},dir_mode=0777,file_mode=0777" >> /etc/fstab
-echo "${dependencieshare} /mnt/gtdependencies cifs vers=3.0,username=${shareaccount},password=${sharekey},dir_mode=0777,file_mode=0777" >> /etc/fstab
+echo "${dependenciesshare} /mnt/gtdependencies cifs vers=3.0,username=${shareaccount},password=${sharekey},dir_mode=0777,file_mode=0777" >> /etc/fstab
 mount -a
 sleep 10  
