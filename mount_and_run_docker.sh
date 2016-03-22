bef8997b7e99967a98f464db8a1cda4cbe88653d
Michael S. Hansen <michael.hansen@nih.gov>
2016-03-22 11:27:16 -0400

    Switched to premade disk image

--- mount_and_run_docker.sh
+++ mount_and_run_docker.sh
@@ -4,6 +4,7 @@ logshare=$1
 dependenciesshare=$2
 shareaccount=$3
 sharekey=$4
+relay_ip=$5
 
 apt-get -qq install cifs-utils -y
 mkdir -p /mnt/gtlog
@@ -12,3 +13,10 @@ echo "${logshare} /mnt/gtlog cifs vers=3.0,username=${shareaccount},password=${s
 echo "${dependenciesshare} /mnt/gtdependencies cifs vers=3.0,username=${shareaccount},password=${sharekey},dir_mode=0777,file_mode=0777" >> /etc/fstab
 mount -a
 sleep 10  
+mkdir -p /mnt/gtlog/$(hostname)
+
+#Restart needed of docker needed after mounting drive
+service docker restart
+
+#Now run container
+docker run -e "GADGETRON_RELAY_IP=${relay_ip}" -v /mnt/gtlog/$(hostname):/tmp -v /mnt/gtdependencies:/tmp/gadgetron --name=gadgetron_container --publish=9002:9002 --publish=8002:8002 --publish=18002:18002 --publish=9080:9080 --restart=unless-stopped --detach -t current_gadgetron
--- setup_relay.sh
+++ setup_relay.sh
@@ -5,8 +5,8 @@ dependenciesshare=$2
 storageaccount=$3
 share_key=$4
 
-echo "sh mount_shares.sh $logshare $dependenciesshare $storageaccount $share_key"
-sh mount_shares.sh $logshare $dependenciesshare $storageaccount $share_key
+echo "sh mount_and_run_docker.sh $logshare $dependenciesshare $storageaccount $share_key 192.168.1.1"
+sh mount_shares.sh $logshare $dependenciesshare $storageaccount $share_key 192.168.1.1
 
 apt-get update
 apt-get install -y nodejs-legacy jq libxml2-utils emacs wget curl
@@ -17,7 +17,7 @@ sh azure_login.sh
 
 mkdir -p /usr/local/share/gadgetron/azure
 cd /usr/local/share/gadgetron/azure
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/mount_shares.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/master/mount_and_run_docker.sh
 wget https://raw.githubusercontent.com/hansenms/azure_templates/master/update_iptables.sh
 wget https://raw.githubusercontent.com/hansenms/azure_templates/master/update_iptables_relay.sh
 wget https://raw.githubusercontent.com/hansenms/azure_templates/master/get_custom_data.sh
