09b6232ecef40af72203b6b4cbe32b1dea2c91e6
Michael S. Hansen <michael.hansen@nih.gov>
2016-04-08 23:42:00 -0400

    Added timestamp to vmss capacity deployment

--- set_vmss_capacity.sh
+++ set_vmss_capacity.sh
@@ -4,6 +4,10 @@ group_name=$1
 vmss_name=$2
 capacity=$3
 
+timestamp() {
+  date +"%H%M%S"
+}
+
 sku_name=$(azure vmss get ${group_name} ${vmss_name} --json | jq .sku.name | tr -d '"')
 
-azure group deployment create -g ${group_name} -m Incremental --template-file vmss_update.json -p "{\"vmssname\" : {\"value\": \"${vmss_name}\"}, \"vmsize\": {\"value\": \"${sku_name}\"}, \"capacity\" : {\"value\": ${capacity}}}"
+azure group deployment create -g ${group_name} -n vmss_update$(timestamp) -m Incremental --template-file vmss_update.json -p "{\"vmssname\" : {\"value\": \"${vmss_name}\"}, \"vmsize\": {\"value\": \"${sku_name}\"}, \"capacity\" : {\"value\": ${capacity}}}"
