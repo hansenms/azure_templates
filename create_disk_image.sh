2f22686a390815330f220833419b328165d7b442
Michael S. Hansen <michael.hansen@nih.gov>
2016-05-19 12:50:25 -0400

    Changed script locations to development branch

--- create_disk_image.sh
+++ create_disk_image.sh
@@ -12,7 +12,7 @@ location="eastus"
 
 time azure group create --name ${group_name} --location ${location} --template-file ${template_file} --parameters-file ${template_parameters}
 
-command="wget https://raw.githubusercontent.com/hansenms/azure_templates/master/setup_disk_creator.sh"
+command="wget https://raw.githubusercontent.com/hansenms/azure_templates/development/setup_disk_creator.sh"
 ssh -o StrictHostKeyChecking=no gadgetron@${group_name}vm.${location}.cloudapp.azure.com $command
 
 command="sudo sh ./setup_disk_creator.sh ${docker_username} ${docker_password} ${docker_email} ${docker_image}"
--- setup_relay.sh
+++ setup_relay.sh
@@ -12,22 +12,22 @@ sh azure_login.sh
 
 mkdir -p /usr/local/share/gadgetron/azure
 cd /usr/local/share/gadgetron/azure
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/mount_and_run_docker.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/update_iptables.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/update_iptables_relay.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/get_custom_data.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/azure_login.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/cloud_monitor.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/cloud_monitor.conf
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/get_node_from_ip.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/get_total_nodes.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/get_deallocated_nodes.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/get_instance_id_from_ip.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/get_vmss_capacity.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/increment_vmss_capacity.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/set_vmss_capacity.sh
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/vmss_update.json
-wget https://raw.githubusercontent.com/hansenms/azure_templates/master/delete_failed_nodes.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/mount_and_run_docker.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/update_iptables.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/update_iptables_relay.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/get_custom_data.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/azure_login.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/cloud_monitor.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/cloud_monitor.conf
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/get_node_from_ip.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/get_total_nodes.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/get_deallocated_nodes.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/get_instance_id_from_ip.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/get_vmss_capacity.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/increment_vmss_capacity.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/set_vmss_capacity.sh
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/vmss_update.json
+wget https://raw.githubusercontent.com/hansenms/azure_templates/development/delete_failed_nodes.sh
 
 
 chmod +x cloud_monitor.sh
