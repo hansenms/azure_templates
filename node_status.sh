1887c8012830a9d6891f243422f8aec7e538f9d2
Michael S. Hansen <michael.hansen@nih.gov>
2016-05-20 15:25:31 +0000

    Made changes due to changes in Azure CLI (vmss get no longer exists) and formatting of node status

--- node_status.sh
+++ node_status.sh
@@ -10,8 +10,8 @@ nodes=$(echo $node_list| jq '. | length')
 echo "Status of $group_name, $vmss_name"
 echo "Number of nodes: $nodes\n"
 n=0
-echo "Node \t| Id \t| Node name         \t| IP     \t| Provisioning \t| Extension \t| Logfile                                      \t| Log modified    \t|" 
-echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "Node \t| Id \t| Node name         \t| IP     \t| Provisioning \t| Extension \t| Logfile                                            \t| Log modified    \t|" 
+echo "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 while [ "$n" -lt "$nodes" ]; do
     instanceid=$(echo $node_list | jq .[$n] | jq .instanceId | tr -d '"')
