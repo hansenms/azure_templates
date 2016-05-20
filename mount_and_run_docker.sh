092f05bd369b3da875805927af75a672c7cce6ea
Michael S. Hansen <michael.hansen@nih.gov>
2016-05-20 10:13:59 -0400

    Some fixes to CIFS shares

--- mount_and_run_docker.sh
+++ mount_and_run_docker.sh
@@ -5,8 +5,8 @@ relay_ip=$2
 
 mkdir -p /gtmount/gtlog
 mkdir -p /gtmount/gtdependencies
-echo "//${smbhostname}/gtlog /gtmount/gtlog cifs guest,vers=3.0,dir_mode=0777,file_mode=0777" >> /etc/fstab
-echo "//${smbhostname}/gtdependencies /gtmount/gtdependencies cifs guest,vers=3.0,dir_mode=0777,file_mode=0777" >> /etc/fstab
+echo "//${smbhostname}/gtlog /gtmount/gtlog cifs guest,dir_mode=0777,file_mode=0777" >> /etc/fstab
+echo "//${smbhostname}/gtdependencies /gtmount/gtdependencies cifs guest,dir_mode=0777,file_mode=0777" >> /etc/fstab
 sleep 3
 mount -a
 sleep 10
