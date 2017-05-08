c142d593bc95bb595f4dcf9ac98cc4914fee93e9
Michael S. Hansen <michael.hansen@nih.gov>
2017-05-08 17:29:32 +0000

    setup_relay_raid.sh
    Fixed several minor bugs, added automatic detection of the disks for the raid.

--- setup_relay_raid.sh
+++ setup_relay_raid.sh
@@ -5,11 +5,11 @@ partition_disk()
 
 diskname=$1
 
-cat <<EOF
-EOF | fdisk $diskname
+cat <<EOF | fdisk $diskname
 n
 p
 1
+
 
 t
 fd
@@ -22,13 +22,22 @@ partprobe
 apt-get install -y mdadm
 
 #TODO: search with lshw -class disk to find available drives
-disks="/dev/sdc /dev/sdd /dev/sde /dev/sdf"
+all_disks=$(sudo lshw -class disk|grep "logical name"| grep "/dev/sd" |awk '{print $3}')
+disks=""
+for d in $all_disks; do
+    if [ "$d" != "/dev/sda" ] && [ "$d" != "/dev/sdb" ]; then
+	disks="$disks $d"
+    fi
+done
+
 raidname="/dev/md127"
 partitions=""
 for d in $disks; do 
+    echo "DISK: $d"
     partition_disk $d
     partitions="$partitions ${d}1"
 done
+echo "PARTITIONS: $partitions"
 
 mdadm --create /dev/md127 --level 0 --raid-devices 4 $partitions
 mkfs -t ext4 /dev/md127
@@ -40,4 +49,3 @@ mkdir -p /data
 
 echo "UUID=$disk_uuid  /data  ext4  defaults  0  2" >> /etc/fstab
 mount -a
-
