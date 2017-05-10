96c97091053433145cdb6d33490fefee634ad6f6
Michael S. Hansen <michael.hansen@nih.gov>
2017-05-10 01:25:47 +0000

    Ubuntu 16.04 for base image.

--- setup_disk_creator.sh
+++ setup_disk_creator.sh
@@ -9,22 +9,19 @@ rm -rf /var/lib/apt/lists/*
 rm -rf /var/lib/apt/lists/partial/*
 apt-get clean
 apt-get update
-apt-get install -y apt-transport-https ca-certificates emacs nfs-common nfs-kernel-server jq
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common emacs nfs-common nfs-kernel-server jq
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get purge lxc-docker
-apt-cache policy docker-engine
 sudo apt-get install -y linux-image-extra-$(uname -r)
 apt-get install -y apparmor
-apt-get install -y docker-engine
+apt-get install -y docker-ce
 service docker start
-apt-get install -y curl
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | tee /etc/apt/sources.list.d/azure-cli.list
 apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
 apt-get update && apt-get install -y azure-cli
 apt-get -qq install cifs-utils -y
-apt-get install -y jq
 
 #Download the requested image
 docker login -u $docker_username -p $docker_password
