d68aa2ea8992fdee768eb737b113addcdb3abd80
Michael S. Hansen <michael.schacht.hansen@gmail.com>
2017-06-08 18:16:37 +0000

    Added gnuplot to disk image creation.

--- setup_disk_creator.sh
+++ setup_disk_creator.sh
@@ -9,7 +9,7 @@ rm -rf /var/lib/apt/lists/*
 rm -rf /var/lib/apt/lists/partial/*
 apt-get clean
 apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common emacs nfs-common nfs-kernel-server jq
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common emacs nfs-common nfs-kernel-server jq gnuplot
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
