0f7a4185f58f71ea530f42447fbc50bf2db11bfa
Michael S. Hansen <michael.schacht.hansen@gmail.com>
2017-06-22 15:29:08 +0000

    send_summary_email.sh: Make sure src id of image is unique too.

--- send_summary_email.sh
+++ send_summary_email.sh
@@ -12,7 +12,7 @@ $BASEDIR/plot_nodes.sh
 img_base64=$(base64 --wrap 0 node_plot.png)
 node_stats=$(bash ${BASEDIR}/node_summary_statistics.sh | tr -d '\n')
 
-data="{\"personalizations\": [{\"to\": ${to} }], \"from\": ${from},\"subject\": \"Gadgetron Cloud Summary ($h)\",\"content\": [{\"type\": \"text/html\", \"value\": \"<h1>Node Summary Statistics</h1>${node_stats}<img src='cid:ii_nodeplot'/>\"}],\"attachments\": [{\"content\": \"$img_base64\", \"type\": \"image/png\", \"filename\": \"${plotfilename}\", \"disposition\":\"inline\",\"content_id\":\"ii_nodeplot\"}]}"
+data="{\"personalizations\": [{\"to\": ${to} }], \"from\": ${from},\"subject\": \"Gadgetron Cloud Summary ($h)\",\"content\": [{\"type\": \"text/html\", \"value\": \"<h1>Node Summary Statistics</h1>${node_stats}<img src='cid:ii_nodeplot${plotfilename}'/>\"}],\"attachments\": [{\"content\": \"$img_base64\", \"type\": \"image/png\", \"filename\": \"${plotfilename}\", \"disposition\":\"inline\",\"content_id\":\"ii_nodeplot${plotfilename}\"}]}"
 
 curl --request POST --url https://api.sendgrid.com/v3/mail/send --header "Authorization: Bearer $sendgrid_api_key" --header 'Content-Type: application/json' --data "$data"
 
